9da2c9448d5983ec876a9af8e7bc18a248d62fe9
ChandanaB5420 <chandanac653@gmail.com>
2024-09-02 12:05:58 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -1,7 +1,6 @@
 #ifndef SOUNDEX_H
 #define SOUNDEX_H
 
-#include "Soundex.h"
 #include <ctype.h>
 #include <string.h>
 
@@ -20,13 +19,20 @@ char getSoundexCode(char c) {
 
 void generateSoundex(const char *name, char *soundex) {
     int len = strlen(name);
+    if (len == 0) {
+        strcpy(soundex, "0000");
+        return;
+    }
+
     soundex[0] = toupper(name[0]);
+    char prevCode = getSoundexCode(name[0]);
     int sIndex = 1;
 
     for (int i = 1; i < len && sIndex < 4; i++) {
         char code = getSoundexCode(name[i]);
-        if (code != '0' && code != soundex[sIndex - 1]) {
+        if (code != '0' && code != prevCode) {
             soundex[sIndex++] = code;
+            prevCode = code;
         }
     }
 
