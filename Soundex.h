be80bffcf1dad8baa3853bb4b4fade637c431f40
ChandanaB5420 <chandanac653@gmail.com>
2024-09-02 12:43:29 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -1,47 +1,40 @@
-#include <iostream>
-using namespace std;
+#ifndef SOUNDEX_H
+#define SOUNDEX_H
 
-char getSoundexCode(char ch) {
-    // This is a placeholder implementation
-    // Replace this with your actual Soundex coding logic
-    // Example mapping, you should adapt it to your actual mapping
-    switch (ch) {
+#include "Soundex.h"
+#include <ctype.h>
+#include <string.h>
+
+char getSoundexCode(char c) {
+    c = toupper(c);
+    switch (c) {
         case 'B': case 'F': case 'P': case 'V': return '1';
         case 'C': case 'G': case 'J': case 'K': case 'Q': case 'S': case 'X': case 'Z': return '2';
         case 'D': case 'T': return '3';
         case 'L': return '4';
         case 'M': case 'N': return '5';
         case 'R': return '6';
-        default: return '0';
+        default: return '0'; // For A, E, I, O, U, H, W, Y
     }
 }
 
-void generateSoundex(const char* name, char* soundex) {
-    char previousCode = '0';
-    int j = 0;
+void generateSoundex(const char *name, char *soundex) {
+    int len = strlen(name);
+    soundex[0] = toupper(name[0]);
+    int sIndex = 1;
 
-    for (int i = 0; name[i] && j < 3; i++) {
-        char currentCode = getSoundexCode(name[i]);
-
-        // Ensure we don't add a '0' code and avoid consecutive duplicates
-        if (currentCode != '0' && currentCode != previousCode) {
-            soundex[j++] = currentCode;
-            previousCode = currentCode;
+    for (int i = 1; i < len && sIndex < 4; i++) {
+        char code = getSoundexCode(name[i]);
+        if (code != '0' && code != soundex[sIndex - 1]) {
+            soundex[sIndex++] = code;
         }
     }
 
-    // Pad with '0's if less than 3 characters
-    while (j < 3) {
-        soundex[j++] = '0';
+    while (sIndex < 4) {
+        soundex[sIndex++] = '0';
     }
 
-    soundex[j] = '\0'; // Null-terminate the result
+    soundex[4] = '\0';
 }
 
-int main() {
-    char name[] = "Example";
-    char soundex[4]; // Allocate space for 3 characters + null terminator
-    generateSoundex(name, soundex);
-    cout << "Soundex: " << soundex << endl;
-    return 0;
-}
+#endif // SOUNDEX_H
